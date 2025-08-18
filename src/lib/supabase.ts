2807e3bd919c29fbc27bb2031e50665d33c852e3
Srinivas Vijayakumar <atvdru@gmail.com>
2025-08-19 02:10:39 +0530

    Added edge functions for claim prompt and email OTP

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,6 +4,7 @@ const supabaseUrl = 'https://syvkefpljdcoricvmqcy.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5dmtlZnBsamRjb3JpY3ZtcWN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MTY5NTYsImV4cCI6MjA2NTQ5Mjk1Nn0.1LulUdvJfybA-rHcag_cCpeO2j2Qt0pQ8XSKWt8huBA'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabaseToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5dmtlZnBsamRjb3JpY3ZtcWN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MTY5NTYsImV4cCI6MjA2NTQ5Mjk1Nn0.1LulUdvJfybA-rHcag_cCpeO2j2Qt0pQ8XSKWt8huBA'
 
 export interface Prompt {
   id: number
